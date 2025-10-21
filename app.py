cd5138e4bc834c7d277385252d0d1c7f804708e0
Badramrajuvyshnavi <1357vyshnavi@gmail.com>
2025-10-22 01:16:03 +0530

    Commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+birthdays = []  # simple in-memory list
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        date = request.form.get('date')
+        if name and date:
+            birthdays.append({'name': name, 'date': date})
+        return redirect('/')
+    return render_template('index.html', birthdays=birthdays)
+
+if __name__ == '__main__':
+    app.run(debug=True)
