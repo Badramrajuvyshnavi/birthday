bfeb619286012234b220f3db17504f05d2a853e5
Badramrajuvyshnavi <143017660+Badramrajuvyshnavi@users.noreply.github.com>
2025-10-22 13:05:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def home():
     return render_template('index.html', birthdays=birthdays)
 
 if __name__ == '__main__':
-    app.run(debug=True,port="5000")
+    app.run(host='0.0.0.0',debug=True,port="5000")
